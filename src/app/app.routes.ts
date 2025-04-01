0742581a16563b027b24e252f0e76fa7e7fa2c2f
tejatriwish <116055791+TejaGanta-Dev@users.noreply.github.com>
2025-04-01 15:18:56 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('../components/dashboard/dashboard.component').then(
+        (m: { DashboardComponent: any }) => m.DashboardComponent
+      ),
+  },
+  {
+    path: 'orders',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('../components/orders/orders.component').then(
+        (m) => m.OrdersComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('../components/dummypage/dummypage.component').then(
+        (m) => m.DummypageComponent
+      ),
+  },
+];
---
+++ src/components/dummypage/dummypage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-dummypage',
+  imports: [RouterLink],
+  templateUrl: './dummypage.component.html',
+  styleUrl: './dummypage.component.css'
+})
+export class DummypageComponent {
+
+}
